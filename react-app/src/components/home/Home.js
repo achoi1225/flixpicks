f351d761d5eaeb454810a752cfb82dfec1a8f156
Andrew Choi <adc1225.dev@gmail.com>
2021-01-14 16:07:46 -0700

    working out bugs in carousel

--- react-app/src/components/home/Home.js
+++ react-app/src/components/home/Home.js
@@ -47,52 +47,12 @@ const Home = ({
                 movieIdx ++;
                 if (movieIdx === mostPopularMovies.length) break;
             }
-            cards.push(<a href={`#section${j+2}`} className="arrow__btn">›</a>)
+            if(j+1 !== sectionNum) {
+                cards.push(<a href={`#section${j+2}`} className="arrow__btn">›</a>)
+            }
             sections.push(<section key={`${j+1}`} id={`section${j+1}`}>{cards}</section>)
         }
         return sections;
-        // console.log("SECTIONS!!! ", sections)
-
-
-        // <section id="section1">
-        //             <a href="#section3" class="arrow__btn">‹</a>
-
-        //             <div className="item" style={{ backgroundImage: `url("https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABZEK-7pZ1H5FD4cTyUb9qB_KeyJGz5p-kfPhCFv4GU_3mbdm8Xfsy4IBchlG9PFNdGff8cBNPaeMra72VFnot41nt0y3e8RLgaVwwh3UvyM2H2_MkmadWbQUeGuf811K7-cxJJh7gA.jpg")`}}>
-        //             </div>
-
-        //             <div className="item" style={{ backgroundImage: `url("https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABZEK-7pZ1H5FD4cTyUb9qB_KeyJGz5p-kfPhCFv4GU_3mbdm8Xfsy4IBchlG9PFNdGff8cBNPaeMra72VFnot41nt0y3e8RLgaVwwh3UvyM2H2_MkmadWbQUeGuf811K7-cxJJh7gA.jpg")`}}>
-        //             </div>
-
-        //             <div className="item" style={{ backgroundImage: `url("https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABZEK-7pZ1H5FD4cTyUb9qB_KeyJGz5p-kfPhCFv4GU_3mbdm8Xfsy4IBchlG9PFNdGff8cBNPaeMra72VFnot41nt0y3e8RLgaVwwh3UvyM2H2_MkmadWbQUeGuf811K7-cxJJh7gA.jpg")`}}>
-        //             </div>
-
-        //             <div className="item" style={{ backgroundImage: `url("https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABZEK-7pZ1H5FD4cTyUb9qB_KeyJGz5p-kfPhCFv4GU_3mbdm8Xfsy4IBchlG9PFNdGff8cBNPaeMra72VFnot41nt0y3e8RLgaVwwh3UvyM2H2_MkmadWbQUeGuf811K7-cxJJh7gA.jpg")`}}>
-        //             </div>
-
-        //             <div className="item" style={{ backgroundImage: `url("https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABZEK-7pZ1H5FD4cTyUb9qB_KeyJGz5p-kfPhCFv4GU_3mbdm8Xfsy4IBchlG9PFNdGff8cBNPaeMra72VFnot41nt0y3e8RLgaVwwh3UvyM2H2_MkmadWbQUeGuf811K7-cxJJh7gA.jpg")`}}>
-        //             </div>
-        //             <a href="#section2" class="arrow__btn">›</a>
-        //         </section>
-
-        //         <section id="section2">
-        //             <a href="#section1" class="arrow__btn">‹</a>
-
-        //             <div className="item" style={{ backgroundImage: `url("https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABZEK-7pZ1H5FD4cTyUb9qB_KeyJGz5p-kfPhCFv4GU_3mbdm8Xfsy4IBchlG9PFNdGff8cBNPaeMra72VFnot41nt0y3e8RLgaVwwh3UvyM2H2_MkmadWbQUeGuf811K7-cxJJh7gA.jpg")`}}>
-        //             </div>
-
-        //             <div className="item" style={{ backgroundImage: `url("https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABZEK-7pZ1H5FD4cTyUb9qB_KeyJGz5p-kfPhCFv4GU_3mbdm8Xfsy4IBchlG9PFNdGff8cBNPaeMra72VFnot41nt0y3e8RLgaVwwh3UvyM2H2_MkmadWbQUeGuf811K7-cxJJh7gA.jpg")`}}>
-        //             </div>
-
-        //             <div className="item" style={{ backgroundImage: `url("https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABZEK-7pZ1H5FD4cTyUb9qB_KeyJGz5p-kfPhCFv4GU_3mbdm8Xfsy4IBchlG9PFNdGff8cBNPaeMra72VFnot41nt0y3e8RLgaVwwh3UvyM2H2_MkmadWbQUeGuf811K7-cxJJh7gA.jpg")`}}>
-        //             </div>
-
-        //             <div className="item" style={{ backgroundImage: `url("https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABZEK-7pZ1H5FD4cTyUb9qB_KeyJGz5p-kfPhCFv4GU_3mbdm8Xfsy4IBchlG9PFNdGff8cBNPaeMra72VFnot41nt0y3e8RLgaVwwh3UvyM2H2_MkmadWbQUeGuf811K7-cxJJh7gA.jpg")`}}>
-        //             </div>
-
-        //             <div className="item" style={{ backgroundImage: `url("https://occ-0-243-299.1.nflxso.net/dnm/api/v5/rendition/412e4119fb212e3ca9f1add558e2e7fed42f8fb4/AAAABZEK-7pZ1H5FD4cTyUb9qB_KeyJGz5p-kfPhCFv4GU_3mbdm8Xfsy4IBchlG9PFNdGff8cBNPaeMra72VFnot41nt0y3e8RLgaVwwh3UvyM2H2_MkmadWbQUeGuf811K7-cxJJh7gA.jpg")`}}>
-        //             </div>
-        //             {/* <a href="#section3" class="arrow__btn">›</a> */}
-        //         </section>
     }
 
     return (
@@ -102,34 +62,20 @@ const Home = ({
                 {mostPopularMovies ? createCarousel() : null}
             </div>
 
-            {/* <div className="category-container">
-                {mostPopularMovies && mostPopularMovies.map(movie => {
-                    return (
-                        <div key={movie.id} className="card-container">
-                            <div className="poster" style={{ backgroundImage: `url(${ movie.image })`}}>
-                            </div>
-                            <div className="title-container">
-                                <div className="movie-title">{movie.title}</div>
-                            </div>
-                        </div>
-                    )
-                })}
-            </div> */}
-
-            {/* <h1>Best Picture</h1>
+            <h1>Best Picture</h1>
             <div className="category-container">
                 {bestPictureMovies && bestPictureMovies.map(movie => {
                     return (
                         <div key={movie.id} className="card-container">
                             <div className="poster" style={{ backgroundImage: `url(${ movie.image })`}}>
-                            </div>
-                            <div className="title-container">
-                                <div className="movie-title">{movie.title}</div>
+                                <div className="title-container">
+                                    <div className="movie-title">{movie.title}</div>
+                                </div>
                             </div>
                         </div>
                     )
                 })}
-            </div> */}
+            </div>
         </div>
     );
 };
