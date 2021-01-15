3784ee1c67be9395adc679eabbaac1b2ecfddcb5
Andrew Choi <adc1225.dev@gmail.com>
2021-01-15 14:33:34 -0700

    worked out styling issues in navbar

--- react-app/src/components/search/Search.js
+++ react-app/src/components/search/Search.js
@@ -19,49 +19,10 @@ const Search = ({clearSearch, lastSearch, setLastSearch}) => {
     setInputValue(e.target.value)
   }
 
-  // const doSearch = (e) => {
-  //   e.stopPropagation();
-  //   const value = document.getElementById('search').value;
-  //   if (value) {
-  //     setLastSearch(value);
-  //     setResults([]);
-  //     setNoResultsMessage(false);
-  //     setDoingSearch(true);
-  //     sizeResults('min');
-  //     (async () => {
-  //     })();
-  // }
-
-  // const displaySearchInfo = (e) => {
-  //   console.log('clicked!!!')
-  //   document.getElementById('search-results').style.display = 'block';
-  //   document.getElementById('search').value = inputValue
-  //   sizeResults(results.length ? 'full' : 'min');
-
-  // }
-
-  // const hideSearchInfo = (e) => {
-  //   e.stopPropagation()
-  //   document.getElementById('search').value = lastSearch
-  //   clearSearch(e);
-  // }
-
-  // const sizeResults = (size) => {
-  //   if (size === 'min') {
-  //     document.getElementById('search-results').style.bottom = 'auto';
-  //   } else {
-  //     document.getElementById('search-results').style.bottom = null;
-  //   }
-  // }
-
-
-
-
   return (
-    // <div className="search-container" onClick={displaySearchInfo} >
     <form className="search-container" onSubmit={rerouteSearchResults}>
-        <input id="search" type="search" className="search-bar" placeholder="search" autoComplete="off" onChange={updateInput} value={inputValue}/>
-      {/* <button className="search-button" onClick={doSearch}><i className="fas fa-search"></i></buttons> */}
+      <input id="search" type="search" className="search-bar" placeholder="search" autoComplete="off" onChange={updateInput} value={inputValue}/>
+      <button className="search-button" onClick={rerouteSearchResults}><i className="fas fa-search"></i></button> 
     </form>
   )
 }
